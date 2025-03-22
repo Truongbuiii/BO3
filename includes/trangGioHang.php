3faf967b89918b392644079d79983b9b13caf837
Truongbuiii <ductruongbui05@gmail.com>
2025-03-22 20:54:41 +0700

    up

--- includes/trangGioHang.php
+++ includes/trangGioHang.php
@@ -7,7 +7,7 @@
     <title>Giỏ hàng</title>
     
     <!-- Bootstrap & FontAwesome -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     
     <!-- Style chung -->
