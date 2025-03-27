f3e0267dff1423f64e9005642a7b562404e28736
Truongbuiii <ductruongbui05@gmail.com>
2025-03-27 14:18:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
       <div class="header_section">
          <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <a class="navbar-brand"href="index.html"><img src="images/logo.png"></a>
+               <a class="navbar-brand"href="index.php"><img src="images/logo.png"></a>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
@@ -59,16 +59,16 @@
                   <ul class="navbar-nav">
 
                      <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Trang chủ</a>
+                        <a class="nav-link" href="/index.php">Trang chủ</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="icecream.html">Kem ly</a>
+                        <a class="nav-link" href="/kemLy.php">Kem ly</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="icecream.html">Kem ốc quế</a>
+                        <a class="nav-link" href="/kemOcQue.php">Kem ốc quế</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="icecream.html">Kem que</a>
+                        <a class="nav-link" href="/kemQue.php">Kem que</a>
                      </li>
                      
                   </ul>
