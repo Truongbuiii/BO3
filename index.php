ae43350eec66ff43c49b221f72fe2eb9b2282333
anhtram14 <anhtram1401@gmail.com>
2025-03-27 19:44:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
         <div class="header_section">
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand"href="index.html"><img src="images/logo.png"></a>
+                <a class="navbar-brand"href="index.php"><img src="images/logo.png"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
@@ -59,7 +59,7 @@
                     <ul class="navbar-nav">
 
                         <li class="nav-item active">
-                            <a class="nav-link" href="index.html">Trang chủ</a>
+                            <a class="nav-link" href="index.php">Trang chủ</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="icecream.html">Kem ly</a>
@@ -84,7 +84,8 @@
 
                         <div class="login_bt"><a href="#"><i class="fa-solid fa-user-large" style="color:#fc95c4; font-size: 150%;"></i></a>
                         <a href="includes/trangGioHang.php">
-                            <i class="bi bi-bag-heart-fill custom-icon"></i>
+                        <i class="bi bi-bag-heart-fill custom-icon"></i>
+
                         </a>
                 
 
