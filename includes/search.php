af5a9dfface7eb960ab064a6315488e162388a8b
Truongbuiii <ductruongbui05@gmail.com>
2025-04-23 20:12:03 +0700

    Update search.php

--- includes/search.php
+++ includes/search.php
@@ -125,7 +125,7 @@ $category_result = $conn->query($category_sql);
       <div class="header_section header_bg">
          <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <a class="navbar-brand"href="index.html"><img src="/images/logo.png"></a>
+               <a class="navbar-brand"href="/index.php"><img src="/images/logo.png"></a>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
@@ -135,13 +135,13 @@ $category_result = $conn->query($category_sql);
                         <a class="nav-link" href="/index.php">Trang chủ</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="about.html">Kem Ly</a>
+                        <a class="nav-link" href="kemLy.php">Kem Ly</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="icecream.html">Kem Ốc Quế</a>
+                        <a class="nav-link" href="kemOcQue.php">Kem Ốc Quế</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="services.html">Kem Que</a>
+                        <a class="nav-link" href="kemQue.php">Kem Que</a>
                      </li>
                     
                   </ul>
