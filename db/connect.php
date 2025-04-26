fcd96e84f2009a6d4973c484ff73e53ffadcc4af
anhtram14 <anhtram1401@gmail.com>
2025-04-26 23:56:05 +0700

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,10 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Mặc định của XAMPP
-$password = ""; // XAMPP không có mật khẩu mặc định
-$dbname = "tiemkem";
+$username = "b03u"; // Mặc định của XAMPP
+$password = "1PsigViV46VdRyal"; // XAMPP không có mật khẩu mặc định
+$dbname = "b03db";
 
 // Kết nối MySQL
-$conn =  mysqli_connect("localhost", "root","", "tiemkem");
+$conn =  mysqli_connect("localhost", "b03u","1PsigViV46VdRyal", "b03db");
 
+?>
